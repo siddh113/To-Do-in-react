6efa0729c7dd86ee6bb33dc10842586194338e9c
siddh113 <siddharthketan@gmail.com>
2024-07-24 18:30:00 +0530

    Form completion

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,18 @@
 import React, {useState} from 'react'
 
-export const TodoForm = () => {
-    const {getValue, setValue} = useState("")
+export const TodoForm = ({addTodo}) => {
+    const [value, setValue] = useState("")
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        addTodo(value); // Adding values from To-do wrapper to to-do form.
+        setValue(""); // Setting the value to empty stringafter submitting form.
+
+        //console.log(value); ----- Checking if the value is getting captured.
+    }
   return (
-    <form className='TodoForm'>
-        <input type='text' className='todo-input' 
+    <form className='TodoForm' onSubmit = {handleSubmit}>
+        <input type='text' className='todo-input' value={value}
         placeholder='What is the task today?' onChange={(e) => setValue(e.target.value)} />
 
         <button type='submit' className='todo-btn'>Add Task</button>
